dcde6305f74cb7dea302f3b3072197b52012c3e8
freestudentsinghub <masha1207355@gmail.com>
2024-08-13 11:43:38 +0300

    add main func

---
+++ src/main.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import json
+
+
+from src.views import (currency_rates_eur, currency_rates_usd,
+                              for_each_card, get_greeting, get_stock_prices,
+                              symbols, top_transactions_by_payment_amount,
+                              transactions)
+
+
+
+
+def main_func(datetime_str: str) -> str:
+    """Главная функция, принимающая строку с датой и временем и возвращающая JSON-ответ."""
+    greeting = get_greeting(datetime_str)
+    cards, total_spend, cashback = for_each_card(transactions)
+    top_five_transactions = top_transactions_by_payment_amount(transactions)
+    currency_rates_one = currency_rates_usd()
+    currency_rates_two = currency_rates_eur()
+    stock_prices = get_stock_prices(symbols)
+
+    response = {
+        "greeting": greeting,
+        "cards": [
+            {
+                "last_digits": cards[0],
+                "total_spent": total_spend[0],
+                "cashback": cashback[0],
+            },
+            {
+                "last_digits": cards[1],
+                "total_spent": total_spend[1],
+                "cashback": cashback[1],
+            },
+        ],
+        "top_transactions": top_five_transactions,
+        "currency_rates": [
+            {"currency": "USD", "rate": currency_rates_one},
+            {"currency": "EUR", "rate": currency_rates_two},
+        ],
+        "stock_prices": stock_prices,
+    }
+
+    return json.dumps(response, ensure_ascii=False, indent=2)
+
+
+print(main_func("31.12.2021 16:44:00"))
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import json
 from typing import Any, Hashable
 
 import pandas as pd
@@ -12,3 +13,24 @@ def read_excel(path: str) -> list[dict[Hashable, Any]]:
 
 
 # print(read_excel('../data/operations.xls'))
+
+def func_user_currencies():
+    """достает из файла данные о 'пользовательских настройках' для курса валют"""
+    with open("../user_settings.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    return data["user_currencies"]
+
+
+user_currencies = func_user_currencies()
+
+
+def func_user_stocks():
+    """достает из файла данные о 'пользовательских настройках' для стоимости акций"""
+    with open("../user_settings.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    return data["user_stocks"]
+
+
+user_stocks = func_user_stocks()
